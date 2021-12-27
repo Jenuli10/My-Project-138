c928cfafc9d846df95fd4c1994f8d4230c8d9711
Jenuli10 <79914668+Jenuli10@users.noreply.github.com>
2021-12-27 11:29:52 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 
 /*created by prashant shukla */
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent("canvas");
+
+	video = createCapture(VIDEO);
+	video.size(800, 400);
+	video.parent("game_console");
+
+	poseNet  = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+	console.log('Model Loaded');
+	}
+
+
 
 var paddle2 =10,paddle1=10;
 
